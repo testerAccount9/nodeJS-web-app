63cfa0d9fc021cd1c7290752630f84ff5af5a36b
Dwip Hazra <55629302+hazradwip@users.noreply.github.com>
2023-12-28 18:20:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world Merry Christmas\n');
+  res.send('Hello world from jenkins build\n');
 });
 
 app.listen(PORT, HOST);
