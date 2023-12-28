ec5b64e09b952b1efef98b1cbd3b2c71efe96ea5
testerAccount9 <152436450+testerAccount9@users.noreply.github.com>
2023-12-28 09:50:10 -0800

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world from jenkins build\n');
+  res.send('Welcome\n');
 });
 
 app.listen(PORT, HOST);
